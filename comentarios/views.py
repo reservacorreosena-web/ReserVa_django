175208041371dab0f1cb4e1967e74b4953ec8cb6
bryannwww <bryanpdeossa@gmail.com>
2026-08-27 00:44:32 -0500

    se agregan estados de reserva, imagenes a la carta y se actualiza login y register

--- comentarios/views.py
+++ comentarios/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from usuarios.decorador import verificar
 
 
+<<<<<<< HEAD
 
 from .serializador import *
 from rest_framework import viewsets
@@ -18,6 +19,8 @@ from rest_framework.permissions import IsAuthenticated
 @verificar
 
 
+=======
+>>>>>>> 1781963 (se agregan estados de reserva, imagenes a la carta y se actualiza login y register)
 def crear_resena(request):
     # Atrapamos los datos basicos del formulario
     if request.method == "POST":
