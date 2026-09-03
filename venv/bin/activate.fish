490c7c18a20f7b54d8d50fe463d63f2047c0f71c
Jhonlondonocloud <jhonstevenparejalondono@gmail.com>
2026-09-03 15:21:07 -0500

    Se crea la funcion comandar en mesa

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/tarde/Escritorio/reserva/ReserVa_django/venv
+set -gx VIRTUAL_ENV /home/tarde/Escritorio/ReserVA/ReserVa_django/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
