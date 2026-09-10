ec121a3a484ff7966fe09559b26b26f55f7ed16e
bryannwww <bryanpdeossa@gmail.com>
2026-09-10 14:37:15 -0500

    Se arregla el preordenar platos y se empieza el cierre de caja

--- contabilidad/urls.py
+++ contabilidad/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('gastos/', views.control_gastos, name='control_gastos'),
     path('guardar_gasto/', views.guardar_gasto, name='guardar_gasto'),
     path('eliminar_gasto/<int:id>/', views.eliminar_gasto, name='eliminar_gasto'),
-    path('editar_gasto/<int:id>/', views.editar_gasto, name='editar_gasto')
+    path('editar_gasto/<int:id>/', views.editar_gasto, name='editar_gasto'),
+    path('inicio_cierre_caja/', views.cierre_caja, name='inicio_cierre_caja'),
 ]
