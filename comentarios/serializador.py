f6f426ce2930f54f962fb69f628521dc87817e84
Mag-Gonzalez <miguelgonza11425@gmail.com>
2026-09-11 15:15:44 -0500

    seguimos añadiendo apis

--- comentarios/serializador.py
+++ comentarios/serializador.py
@@ -1,9 +1,7 @@
-from .models import *
 from rest_framework import serializers
+from .models import Resena
 
 class ResenaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resena
-        # fields = ['id', 'nombre', 'costo', 'descripcion', 'estado']
-        fields = '__main__'
         fields = '__all__'
--- comentarios/urls.py
+++ comentarios/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 app_name = 'comentarios'
 
+router = DefaultRouter()
+router.register(r'api/resenas', views.ResenaViewSet, basename='api-resenas')
+
 urlpatterns = [
     path('comentarios/', views.crear_resena, name='crear_resena'),
+    
+    path('', include(router.urls)),
 ]
